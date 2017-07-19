d0d8f43eb3dbbb8620b51372c9063e372785a42f
losfair <zhy20000919@hotmail.com>
2017-07-19 21:48:42 +0800

    Add macOS build

--- travis_scripts/osx.sh
+++ travis_scripts/osx.sh
@@ -1,3 +1,4 @@
+brew install xz
 mkdir $HOME/ice-node-lib
 wget -O $HOME/ice-node-lib/ice_core_macos.tar.xz https://github.com/losfair/IceCore/releases/download/v0.1.1-all-2/ice_core_macos.tar.xz
 cd $HOME/ice-node-lib
