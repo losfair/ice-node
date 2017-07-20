4f956b9749307e8174b78fce56d45fa1c1b46179
losfair <zhy20000919@hotmail.com>
2017-07-21 00:24:04 +0800

    Update core version in Travis build scripts

--- travis_scripts/osx.sh
+++ travis_scripts/osx.sh
@@ -1,6 +1,6 @@
 brew install xz
 mkdir $HOME/ice-node-lib
-wget -O $HOME/ice-node-lib/ice_core_macos.tar.xz https://github.com/losfair/IceCore/releases/download/v0.1.1-all-2/ice_core_macos.tar.xz
+wget -O $HOME/ice-node-lib/ice_core_macos.tar.xz https://github.com/losfair/IceCore/releases/download/v0.1.2/ice_core_macos.tar.xz
 cd $HOME/ice-node-lib
 xz -d < ice_core_macos.tar.xz | tar x
 mv dist/* ./
