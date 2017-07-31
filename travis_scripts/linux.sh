963815401a9dc49285396decb211f82096469052
losfair <zhy20000919@hotmail.com>
2017-07-31 17:28:49 +0800

    v0.2.1

--- travis_scripts/linux.sh
+++ travis_scripts/linux.sh
@@ -1,5 +1,5 @@
 mkdir /tmp/ice-node-lib
-wget -O /tmp/ice-node-lib/ice_core_linux.tar.xz https://github.com/losfair/IceCore/releases/download/v0.2.0/ice_core_linux.tar.xz
+wget -O /tmp/ice-node-lib/ice_core_linux.tar.xz https://github.com/losfair/IceCore/releases/download/v0.2.1/ice_core_linux.tar.xz
 cd /tmp/ice-node-lib
 xz -d < ice_core_linux.tar.xz | tar x
 mv dist/* ./
