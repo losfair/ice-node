223f931f53f1eff8df1eae86db29ef2e6cbb697d
losfair <zhy20000919@hotmail.com>
2017-09-17 15:47:59 +0800

    More core tests

--- core_test.js
+++ core_test.js
@@ -8,6 +8,15 @@ core.http_server_start(server);
 
 let rt = core.http_server_route_create("/hello_world", (ctx, req) => {
     let resp = core.http_response_create();
+    core.http_response_set_body(
+        resp,
+        Buffer.from("Hello world!")
+    );
+    core.http_response_set_header(
+        resp,
+        "X-Powered-By",
+        "Ice-node"
+    );
     core.http_server_endpoint_context_end_with_response(
         ctx,
         resp
