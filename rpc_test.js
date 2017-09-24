a9a7b19d3801ac135e866cf790b54bee6dae7dd3
losfair <zhy20000919@hotmail.com>
2017-09-24 17:07:23 +0800

    More tests

--- rpc_test.js
+++ rpc_test.js
@@ -1,10 +1,23 @@
 const lib = require("./lib.js");
 const rpc = lib.rpc;
+const assert = require("assert");
 
 let cfg = new rpc.RpcServerConfig();
+cfg.addMethod("ping", (ctx) => {
+    ctx.end(
+        rpc.RpcParam.buildString("Pong")
+    );
+});
 cfg.addMethod("add", (ctx) => {
     ctx.end(
         rpc.RpcParam.buildI32(ctx.getParam(0).getI32() + ctx.getParam(1).getI32())
+    );
+});
+cfg.addMethod("add_string", (ctx) => {
+    ctx.end(
+        rpc.RpcParam.buildString(
+            ctx.getParam(0).getString() + ctx.getParam(1).getString()
+        )
     );
 });
 
@@ -12,30 +25,56 @@ let server = new rpc.RpcServer(cfg);
 server.start("127.0.0.1:1653");
 
 let client = new rpc.RpcClient("127.0.0.1:1653");
-let maxSeq = 1000000;
-let seq = 0;
+client.connect(async conn => {
+    try {
+        console.log("Connected");
+        await testPing(conn);
+        await testAdd(conn);
+        await testAddString(conn);
+        console.log("Done");
+    } catch(e) {
+        console.log(e);
+    }
+    process.exit(0);
+});
 
-setTimeout(() => {
-    for(let i = 0; i < 32; i++) {
-        client.connect(conn => {
-            console.log("Connected");
-            testCall(conn);
+function testPing(conn) {
+    return new Promise(cb => {
+        conn.call("ping", [], ret => {
+            assert(ret.getString() == "Pong");
+            ret.destroy();
+            console.log("[+] testPing OK");
+            cb();
         });
-    }
-}, 3000);
+    });
+}
 
-function testCall(conn) {
-    if(seq == maxSeq) {
-        process.exit(0);
-    }
-    seq++;
-    conn.call("add", [
-        rpc.RpcParam.buildI32(0),
-        rpc.RpcParam.buildI32(seq)
-    ], ret => {
-        let v = ret.getI32();
-        console.log(v);
-        ret.destroy();
-        testCall(conn);
+function testAdd(conn) {
+    return new Promise(cb => {
+        conn.call("add", [
+            rpc.RpcParam.buildI32(1),
+            rpc.RpcParam.buildI32(2)
+        ], ret => {
+            let v = ret.getI32();
+            assert(v === 3);
+            ret.destroy();
+            console.log("[+] testAdd OK");
+            cb();
+        });
+    });
+}
+
+function testAddString(conn) {
+    return new Promise(cb => {
+        conn.call("add_string", [
+            rpc.RpcParam.buildString("Hello "),
+            rpc.RpcParam.buildString("world")
+        ], ret => {
+            let v = ret.getString();
+            assert(v === "Hello world");
+            ret.destroy();
+            console.log("[+] testAddString OK");
+            cb();
+        });
     });
 }
